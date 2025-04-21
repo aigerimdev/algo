4bece27d71186eade512e7140b5d22afc2762270
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-20 23:51:13 -0700

    count_occurance function was added

---
+++ rotate.py
@@ -0,0 +1,10 @@
+def count_occur(inp, target):
+    
+    counter = 0 
+    for char in inp:
+        if char == target:
+            counter += 1
+    return counter
+    
+print(count_occur([1, 2, 3, 2, 5, 2], 2))
+print(count_occur("aigerim", "i"))

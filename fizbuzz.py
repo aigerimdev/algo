e3bc76922e049f3879995abefd29fd555c1c5e19
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-22 23:40:08 -0700

    fizzBuzz algorithm was added

---
+++ fizbuzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(n):
+    for number in range(1, n + 1):
+        if number % 15 == 0:
+            print("FizzBuzz")
+        if number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+print(fizz_buzz(30))
--- pair.py
+++ pair.py
@@ -4,11 +4,9 @@ def count_pairs(arr, target):
 
     # Iterate through each element in the array
     for i in range(n):
-      
         # For each element arr[i], check every
         # other element arr[j] that comes after it
         for j in range(i + 1, n):
-          
             # Check if the sum of the current pair
             # equals the target
             if arr[i] + arr[j] == target:

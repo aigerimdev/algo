c92976b4be5d7b176c51900e138150d15788cd4c
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-24 21:14:37 -0700

    new iplementations

--- intersection.py
+++ intersection.py
@@ -7,3 +7,40 @@ def get_intersection(list1, list2):
             intersection.append(num)
 
     return intersection
+
+
+#An example of a working implementation:
+
+def get_intersection(red, blue):
+    frequency_hash = {}
+
+    for list in [red, blue]:
+        for item in list:
+            if item in frequency_hash:
+                frequency_hash[item] += 1
+            else:
+                frequency_hash[item] = 1
+
+    intersections = []
+    for item, quantity in frequency_hash.items():
+        if quantity > 1:
+            intersections.append(item)
+
+    return intersections
+
+#Another example of a working implementation:
+
+def get_intersection(red, blue):
+    frequency_hash = {}
+
+    for list in [red, blue]:
+        for item in list:
+            count = frequency_hash.get(item, 0)
+            frequency_hash[item] = count + 1
+
+    intersections = []
+    for item, quantity in frequency_hash.items():
+        if quantity > 1:
+            intersections.append(item)
+
+    return intersections

5af6c980246ff35fc73dd8bcac01d5143685b346
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-06-22 01:13:53 -0700

    adds common prefix algo

---
+++ longest_common_prefix.py
@@ -0,0 +1,28 @@
+
+# 14. Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+def longest_common_prefix(words):
+    common_prefix = ''
+    for chars in zip(*words):
+        single_chars = set(chars)
+        if len(single_chars) == 1:
+            common_prefix += chars[0]
+        else:
+            break
+    return common_prefix
+words = ["flower","flow","flight"]
+print(longest_common_prefix(words))
+    
